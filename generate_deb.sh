3c72b682af7d3410f049cdc0556fe6b961e4681d
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-03 16:23:57 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -6,4 +6,8 @@ chmod +x pld2sobp
 mkdir --parents debpkg/usr/bin
 mv pld2sobp debpkg/usr/bin
 
+# for debian <= 7 no compression and old format
+dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build debpkg pymchelper_old.deb
+
+# for all versions use newest format of deb packages
 dpkg-deb --root-owner-group --build debpkg pymchelper.deb
