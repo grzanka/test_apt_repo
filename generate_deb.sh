a78f1fb1b37adb8fa060f92ced1ffbd3cc81c600
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-03 15:31:38 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/runmc
-mv runmc debpkg
+wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/pld2sobp
+mv pld2sobp debpkg/
 
 dpkg-deb -b debpkg pymchelper.deb
