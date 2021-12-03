6aac2c67126d3133318069315c53002c2c81b522
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-03 15:50:29 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+rm --force pld2sobp
 wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/pld2sobp
-mv pld2sobp debpkg/
+chmod +x pld2sobp
+mkdir --parents debpkg/usr/bin
+mv pld2sobp debpkg/usr/bin
 
-dpkg-deb -b debpkg pymchelper.deb
+dpkg-deb --root-owner-group --build debpkg pymchelper.deb
