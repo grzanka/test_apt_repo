976a85f554b79d1b9a6ccd349cd089470abb4e41
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-24 00:07:19 +0100

    smaller files

--- generate_deb.sh
+++ generate_deb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm --force pld2sobp
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/pld2sobp
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/convertmc
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/runmc
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.4/mcscripter
+wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/pld2sobp
+wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/convertmc
+wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/runmc
+wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/mcscripter
 chmod +x pld2sobp
 chmod +x convertmc
 chmod +x runmc
@@ -13,10 +13,10 @@ mkdir --parents debpkg/usr/bin
 
 # we need smaller size, Github Pages has limit 100 MB
 
-#mv pld2sobp debpkg/usr/bin
-#mv convertmc debpkg/usr/bin
+mv pld2sobp debpkg/usr/bin
+mv convertmc debpkg/usr/bin
 mv runmc debpkg/usr/bin
-#mv mcscripter debpkg/usr/bin
+mv mcscripter debpkg/usr/bin
 
 # for debian <= 7 no compression and old format
 dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build debpkg pymchelper_old.deb
