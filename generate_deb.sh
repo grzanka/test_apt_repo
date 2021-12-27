a6f6b90439b2dc5b8559ad4581fcab6ba8b08868
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-27 22:12:14 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -6,33 +6,35 @@ set -x
 #SCRIPTS_NAMES = ('convertmc' 'runmc' 'pld2sobp' 'mcscripter')
 SCRIPTS_NAMES=('convertmc' 'runmc')
 
-VERSION='1.10.7'
-
 # we need smaller size, Github Pages has limit 100 MB
 for SCRIPT in "${SCRIPTS_NAMES[@]}"
 do
     BIN_DIR=pymchelper-${SCRIPT}/usr/bin
     rm --recursive --force ${BIN_DIR}
     mkdir --parents ${BIN_DIR}
-    wget --quiet https://github.com/DataMedSci/pymchelper/releases/latest/download/${SCRIPT} --output-file=${BIN_DIR}/${SCRIPT} || exit 1;
+
+    # download latest release, exit in case of failure
+    wget --quiet https://github.com/DataMedSci/pymchelper/releases/latest/download/${SCRIPT} --output-document=${BIN_DIR}/${SCRIPT} || exit 1;
     chmod +x ${BIN_DIR}/${SCRIPT}
 
+    VERSION=`${BIN_DIR}/${SCRIPT} --version`
     # adjust version
     sed -i "s/Version\:.*/Version\: ${VERSION}/g" pymchelper-${SCRIPT}/DEBIAN/control
 
-    # for debian <= 7 no compression and old format
+    # for debian <= 7 no compression and old format, exit in case of failure
     dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}-old.deb || exit 1;
 
-    # for all versions use newest format of deb packages
+    # for all versions use newest format of deb packages, exit in case of failure
     dpkg-deb --root-owner-group --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}.deb || exit 1;
 done
 
-# adjust version
+# adjust version, use version of latest script from the loop above
 sed -i "s/Version\:.*/Version\: ${VERSION}/g" pymchelper/DEBIAN/control
+sed -i "s/\(=dummy\)/\(=${VERSION}\)/g" pymchelper/DEBIAN/control
 
-# for debian <= 7 no compression and old format
+# for debian <= 7 no compression and old format, exit in case of failure
 dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper pymchelper-old.deb || exit 1;
-# for all versions use newest format of deb packages
+# for all versions use newest format of deb packages, exit in case of failure
 dpkg-deb --root-owner-group --build pymchelper pymchelper.deb || exit 1;
 
 ls -alh *deb
