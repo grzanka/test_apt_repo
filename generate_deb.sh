7acd30caa95658d3802053e0eb8fc40314bab2ec
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-27 21:52:47 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -14,19 +14,25 @@ do
     BIN_DIR=pymchelper-${SCRIPT}/usr/bin
     rm --recursive --force ${BIN_DIR}
     mkdir --parents ${BIN_DIR}
-    wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v${VERSION}/${SCRIPT} --output-file=${BIN_DIR}/${SCRIPT}
+    wget --quiet https://github.com/DataMedSci/pymchelper/releases/latest/download/${SCRIPT} --output-file=${BIN_DIR}/${SCRIPT} || exit 1;
     chmod +x ${BIN_DIR}/${SCRIPT}
 
+    # adjust version
+    sed -i "s/Version\:.*/Version\: ${VERSION}/g" pymchelper-${SCRIPT}/DEBIAN/control
+
     # for debian <= 7 no compression and old format
-    dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}-old.deb
+    dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}-old.deb || exit 1;
 
     # for all versions use newest format of deb packages
-    dpkg-deb --root-owner-group --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}.deb
+    dpkg-deb --root-owner-group --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}.deb || exit 1;
 done
 
+# adjust version
+sed -i "s/Version\:.*/Version\: ${VERSION}/g" pymchelper/DEBIAN/control
+
 # for debian <= 7 no compression and old format
-dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper pymchelper-old.deb
+dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper pymchelper-old.deb || exit 1;
 # for all versions use newest format of deb packages
-dpkg-deb --root-owner-group --build pymchelper pymchelper.deb
+dpkg-deb --root-owner-group --build pymchelper pymchelper.deb || exit 1;
 
 ls -alh *deb
