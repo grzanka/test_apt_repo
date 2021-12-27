e53bdbae14450f5f69dc805c6582ba2f63c64a3a
Leszek Grzanka <grzanka@agh.edu.pl>
2021-12-27 21:02:38 +0000

    test

--- generate_deb.sh
+++ generate_deb.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-rm --force pld2sobp
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/pld2sobp
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/convertmc
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/runmc
-wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v1.10.7/mcscripter
-chmod +x pld2sobp
-chmod +x convertmc
-chmod +x runmc
-chmod +x mcscripter
-mkdir --parents debpkg/usr/bin
+# Print commands and their arguments as they are executed
+set -x
+
+#SCRIPTS_NAMES = ('convertmc' 'runmc' 'pld2sobp' 'mcscripter')
+SCRIPTS_NAMES=('convertmc' 'runmc')
+
+VERSION='1.10.7'
 
 # we need smaller size, Github Pages has limit 100 MB
+for SCRIPT in "${SCRIPTS_NAMES[@]}"
+do
+    BIN_DIR=pymchelper-${SCRIPT}/usr/bin
+    rm --recursive --force ${BIN_DIR}
+    mkdir --parents ${BIN_DIR}
+    wget --quiet https://github.com/DataMedSci/pymchelper/releases/download/v${VERSION}/${SCRIPT} --output-file=${BIN_DIR}/${SCRIPT}
+    chmod +x ${BIN_DIR}/${SCRIPT}
 
-mv pld2sobp debpkg/usr/bin
-mv convertmc debpkg/usr/bin
-mv runmc debpkg/usr/bin
-mv mcscripter debpkg/usr/bin
+    # for debian <= 7 no compression and old format
+    dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}-old.deb
 
-# for debian <= 7 no compression and old format
-dpkg-deb --root-owner-group -Znone --deb-format=0.939000 --build debpkg pymchelper_old.deb
-
-# for all versions use newest format of deb packages
-dpkg-deb --root-owner-group --build debpkg pymchelper.deb
+    # for all versions use newest format of deb packages
+    dpkg-deb --root-owner-group --build pymchelper-${SCRIPT} pymchelper-${SCRIPT}.deb
+done
